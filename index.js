93b15885a835c7a2e799f1dfe303113d92e476c4
Overtorment <overtorment@gmail.com>
2022-05-01 20:06:54 +0100

    feat

--- index.js
+++ index.js
@@ -32,6 +32,44 @@ app.get('/download/:buildid', async function (req, res) {
 	}
 })
 
+async function handleDownloadArtifactForBranch(branch, res) {
+	const url = `https://api.github.com/repos/BlueWallet/BlueWallet/actions/runs?status=success&event=pull_request&branch=${branch}`
+	const result = await fetch(url, {
+		method: 'GET',
+		headers: {
+			"accept": "application/json",
+		},
+	});
+	const json = await result.json()
+
+	let id = false;
+	for (const run of json?.workflow_runs ?? []) {
+		if (run.name === "BuildReleaseApk") {
+			id = run.id;
+		}
+	}
+
+
+	if (id) {
+		res.redirect(307, `https://github.com/BlueWallet/BlueWallet/actions/runs/${id}`);
+	} else {
+		console.warn({json})
+		res.send(json);
+	}
+}
+
+app.get('/downloadArtifactForBranch/:branch', async function (req, res) {
+	return await handleDownloadArtifactForBranch(req.params.branch, res)
+})
+
+app.get('/downloadArtifactForBranch/:branch/:branch2', async function (req, res) {
+	if (req.params.branch2) {
+		return await handleDownloadArtifactForBranch(req.params.branch + '/' + req.params.branch2, res);
+	} else {
+		return await handleDownloadArtifactForBranch(req.params.branch, res);
+	}
+})
+
 app.listen(port, () => {
 	  console.log(`app listening at http://localhost:${port}`)
 })
